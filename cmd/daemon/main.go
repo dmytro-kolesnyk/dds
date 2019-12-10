e289ae48b28a36420852b83ab62af574dc714108
YuriiKachaiev <43471764+YuriiKachaiev@users.noreply.github.com>
2019-12-10 13:51:20 +0200

    Application init (#4)
    * Add daemon start-stop
    
    * Deleted excess func

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/dmytro-kolesnyk/dds/cmd/daemon/app"
 )
 
 func main() {
 	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
+	app := app.NewDaemon()
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("awaiting signal")
-	<-done
+	<-sigs
 	fmt.Println("exiting")
+
+	if err := app.Stop(); err != nil {
+		log.Fatal(err)
+	}
 }
