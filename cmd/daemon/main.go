65a50060d44f99445086ec1413ad83699966f646
YuriiKachaiev <yurii.kachaiev@globallogic.com>
2019-12-10 13:48:48 +0200

    Deleted excess func

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -12,16 +12,8 @@ import (
 
 func main() {
 	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
 
 	app := app.NewDaemon()
 	if err := app.Start(); err != nil {
@@ -29,7 +21,7 @@ func main() {
 	}
 
 	fmt.Println("awaiting signal")
-	<-done
+	<-sigs
 	fmt.Println("exiting")
 
 	if err := app.Stop(); err != nil {
