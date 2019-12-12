464941af6c1f00db8dfe32fc257977364dc33d1e
baixeing <baixeing@trojan.work>
2019-12-12 13:44:59 +0200

    prepare to merge

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/dmytro-kolesnyk/dds/cmd/daemon/app"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	app := app.NewDaemon()
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("awaiting signal")
+	<-sigs
+	fmt.Println("exiting")
+
+	if err := app.Stop(); err != nil {
+		log.Fatal(err)
+	}
 }
