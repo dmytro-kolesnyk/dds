fea1fe8257dda24f1eeeac973f32ecd01e271823
baixeing <baixeing@trojan.work>
2019-12-12 13:16:00 +0200

    prepare to merge

--- cmd/daemon/app/app.go
+++ cmd/daemon/app/app.go
@@ -1,6 +1,34 @@
 package app
 
+import (
+	"github.com/dmytro-kolesnyk/dds/cmd/daemon/cliapi"
+	"log"
+)
+
 type App interface {
 	Start() error
 	Stop() error
+}
+
+type Daemon struct {
+}
+
+func NewDaemon() App {
+	return &Daemon{}
+}
+
+func (rcv *Daemon) Start() error {
+	log.Println("Started")
+
+	cliApi := cliapi.NewCliApi()
+	if err := cliApi.Listen(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (rcv *Daemon) Stop() error {
+	log.Println("Stopped")
+	return nil
 }
