e69f025a7ce128bfcf713e0d9f3910fdcdfd8993
baixeing <baixeing@trojan.work>
2019-12-12 13:09:11 +0200

    communication_server added

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	communicationServer "github.com/dmytro-kolesnyk/dds/communication_server"
 	"github.com/dmytro-kolesnyk/dds/discovery"
 	"github.com/dmytro-kolesnyk/dds/node"
 	"github.com/google/uuid"
@@ -16,17 +17,22 @@ func main() {
 		3451,
 	)
 
-	neighbours := make(chan node.Node)
+	neighbours := make(chan *node.Node)
 	if err := discoverer.Start(neighbours); err != nil {
 		log.Fatalln(err)
 	}
 	defer discoverer.Stop()
 
-	for n := range neighbours {
-		if n.Instance != discoverer.Instance {
-			log.Println(n)
+	/*
+		for n := range neighbours {
+			if n.Instance != discoverer.Instance {
+				log.Println(n)
+			}
 		}
+	*/
+	cs := communicationServer.NewCommunicationServer()
+	if err := cs.Start(":3451", neighbours); err != nil {
+		log.Fatalln(err)
 	}
-
 	select {}
 }
