685ec167f7a9dcaaf3b246ab2ec98dd5a6ef0a3a
GoNaMore <capdev@o2.pl>
2019-12-16 12:21:35 +0200

    Mock CliApi handlers response

--- cmd/daemon/cliapi/handlers/download_handler.go
+++ cmd/daemon/cliapi/handlers/download_handler.go
@@ -11,6 +11,13 @@ type DownloadHandler struct {
 
 func (rcv *DownloadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("download handler")
+
+	response := `{
+	  "uuid": "f4c8de96-4e03-4772-b83c-f8dfbe64e998"
+	}`
+	if _, err := fmt.Fprintf(w, response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func NewDownloadHandler() *DownloadHandler {
--- cmd/daemon/cliapi/handlers/upload_handler.go
+++ cmd/daemon/cliapi/handlers/upload_handler.go
@@ -11,6 +11,13 @@ type UploadHandler struct {
 
 func (rcv *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("upload handler")
+
+	response := `{
+	  "uuid": "f4c8de96-4e03-4772-b83c-f8dfbe64e998"
+	}`
+	if _, err := fmt.Fprintf(w, response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func NewUploadHandler() *UploadHandler {
