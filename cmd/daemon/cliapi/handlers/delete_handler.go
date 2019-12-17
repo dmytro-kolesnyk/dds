1f369f239eb92b9d16d23f61368e8b2d6b4505c2
GoNaMore <57295557+gonamore@users.noreply.github.com>
2019-12-17 12:18:05 +0200

    Mock CliApi handlers response (#8)

--- cmd/daemon/cliapi/handlers/delete_handler.go
+++ cmd/daemon/cliapi/handlers/delete_handler.go
@@ -11,6 +11,13 @@ type DeleteHandler struct {
 
 func (rcv *DeleteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("delete handler")
+
+	response := `{
+	  "uuid": "f4c8de96-4e03-4772-b83c-f8dfbe64e998"
+	}`
+	if _, err := fmt.Fprintf(w, response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func NewDeleteHandler() *DeleteHandler {
--- cmd/daemon/cliapi/handlers/list_handler.go
+++ cmd/daemon/cliapi/handlers/list_handler.go
@@ -11,6 +11,16 @@ type ListHandler struct {
 
 func (rcv *ListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("list handler")
+
+	response := `{
+	   "files": [{
+		  "uuid": "f4c8de96-4e03-4772-b83c-f8dfbe64e998",
+		  "file_name": "file1.avi"
+		}]
+	}`
+	if _, err := fmt.Fprintf(w, response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func NewListHandler() *ListHandler {
