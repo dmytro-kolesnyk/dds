fd88bf74ab1da63ea4ef76834530abb7b9523ee7
antonyuksofi <antonyuksofia27@gmail.com>
2019-12-19 13:34:34 +0200

    added logger

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -8,9 +8,13 @@ import (
 	"syscall"
 
 	"github.com/dmytro-kolesnyk/dds/cmd/daemon/app"
+	"github.com/dmytro-kolesnyk/dds/common/logger"
 )
 
 func main() {
+	logger := logger.NewMainLogger()
+	logger.Info("starting daemon...")
+
 	sigs := make(chan os.Signal, 1)
 
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
--- common/logger/logger.go
+++ common/logger/logger.go
@@ -1,1 +1,35 @@
 package logger
+
+import (
+	"reflect"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Logger struct {
+	ownerName string
+}
+
+func NewMainLogger() *Logger {
+	logger := Logger{""}
+
+	return &logger
+}
+
+func NewLogger(owner interface{}) *Logger {
+	logger := Logger{getTypeName(owner)}
+
+	return &logger
+}
+
+func getTypeName(myvar interface{}) string {
+	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
+		return t.Elem().Name()
+	} else {
+		return t.Name()
+	}
+}
+
+func (logger *Logger) Info(args ...interface{}) {
+	log.Info(logger.ownerName, args)
+}
